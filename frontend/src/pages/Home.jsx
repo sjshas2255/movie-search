885b749bbe327cdb464e7512479804a7080f69bb
sjshas2255 <sharonjoy.sj@gmail.com>
2025-02-15 21:20:59 -0700

    movie-search
    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,87 @@
+
+import MovieCard from '../components/MovieCard'
+import { useState, useEffect } from 'react';
+import "../css/Home.css"
+import { searchMovies, getPopularMovies } from '../services/api';
+
+function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const loadPopularMovies = async () => {
+            try {
+
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            }
+            catch (err) {
+                console.log(err);
+                setError("Failed to load the movies from api...")
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        loadPopularMovies();
+
+    }, [])
+
+
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        if (!searchQuery.trim()) return;
+
+        if (loading) return;
+
+        setLoading(true);
+
+        try {
+            const getSearchResults = await searchMovies(searchQuery);
+            setMovies(getSearchResults);
+            setError(null);
+
+        } catch (err) {
+            console.log(err);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}>
+                </input>
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            {error && <div className="error-message">{error}</div>}
+
+            {loading ?
+                (<div>Loading...</div>) :
+                (<div className="movies-grid">
+                    {movies.map(movie => (
+                        movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
+                    ))}
+
+                </div>
+                )}
+        </div>
+    )
+}
+
+export default Home;
